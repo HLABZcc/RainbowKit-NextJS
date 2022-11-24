8a226e802518822be9250e63cfd518860420a4b7
HLABZ® <113842155+HLABZcc@users.noreply.github.com>
2022-11-24 14:13:41 +0100

    Add files via upload

---
+++ pages/_app.tsx
@@ -0,0 +1,37 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.goerli, chain.polygon, chain.optimism, chain.arbitrum, chain.hardhat],
+  [
+    // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider theme={darkTheme()} chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
+}
